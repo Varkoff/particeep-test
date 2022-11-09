2ad26dc069091597d28fb18d39cbd843c990f9d2
Virgile-Labomode <virgile@labomode.com>
2022-11-09 15:29:44 +0100

    Add Like and Dislike Icons

--- src/Components/MovieItem.tsx
+++ src/Components/MovieItem.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Movie } from '../App';
+import ThumbsDownIcon from './Icons/ThumbsDownIcon';
+import { ThumbsUpIcon } from './Icons/ThumbsUpIcon';
+import TrashIcon from './Icons/TrashIcon';
 
 const MovieItem = ({
 	movie,
@@ -25,40 +28,58 @@ const MovieItem = ({
 	return (
 		<li
 			key={movie.id}
-			className='rounded-lg flex flex-col gap-4 px-8 py-6 bg-white'
+			className='relative h-fit rounded-lg flex flex-col gap-4 px-8 py-6 bg-white'
 		>
 			<span className='text-4xl text-ellipsis max-w-[10ch] overflow-hidden font-sans font-extrabold'>
 				{movie.title}
 			</span>
-			<div className='flex flex-col gap-4'>
-				<span>{movie.category}</span>
-				<div className='flex flex-col gap-4'>
-					<button
-						onClick={
-							isDisliked
-								? () => onRemoveDislike(movie.id)
-								: () => onDislike(movie.id)
-						}
-						className='bg-red-800 text-white text-2xl'
-					>
-						{dislikes} {isDisliked ? '❌' : '👎'}
-					</button>
-					<button
-						onClick={
-							isLiked ? () => onRemoveLike(movie.id) : () => onLike(movie.id)
-						}
-						className='bg-emerald-700 text-white text-2xl'
-					>
-						{likes} {isLiked ? '❤️' : '👍'}
-					</button>
-				</div>
-			</div>
+
 			<button
-				className='mt-auto ml-auto text-red-600'
+				className='group absolute right-6 text-red-600'
 				onClick={() => onDelete(movie.id)}
 			>
-				Supprimer
+				<TrashIcon className='stroke-red-800 group-hover:scale-125 group-hover:rotate-6 transition-all duration-100 ease-in-out' />
 			</button>
+			<div className='mt-auto flex flex-col gap-4'>
+				<span>{movie.category}</span>
+				<div className='flex flex-row gap-4'>
+					<div className='bg-red-50 flex-1 flex gap-8 items-center'>
+						<button
+							onClick={
+								isDisliked
+									? () => onRemoveDislike(movie.id)
+									: () => onDislike(movie.id)
+							}
+						>
+							<ThumbsDownIcon
+								className={`w-12 h-12 duration-100 ease-in-out transition-all  ${
+									isDisliked
+										? 'fill-red-800 stroke-transparent'
+										: 'stroke-gray-700 hover:stroke-red-700'
+								} `}
+							/>
+						</button>
+						<span className='text-4xl font-bold font-sans'>{dislikes}</span>
+					</div>
+
+					<div className='bg-emerald-50 flex-1 flex gap-8 items-center'>
+						<button
+							onClick={
+								isLiked ? () => onRemoveLike(movie.id) : () => onLike(movie.id)
+							}
+						>
+							<ThumbsUpIcon
+								className={`w-12 h-12 duration-100 ease-in-out transition-all  ${
+									isLiked
+										? 'fill-emerald-700 stroke-transparent'
+										: 'stroke-gray-700 hover:stroke-emerald-600'
+								} `}
+							/>
+						</button>
+						<span className='text-4xl font-bold font-sans'>{likes}</span>
+					</div>
+				</div>
+			</div>
 		</li>
 	);
 };
